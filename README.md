126de4e354678bc53c832a72ba09eff9c4602353
unique <641341028@qq.com>
2016-07-30 21:33:28 +0800

    change

--- README.md
+++
@@ -1,2 +0,0 @@
-# daiyiming.github.com
-速算24小游戏，WebApp
---
+++ js/index.js
@@ -0,0 +1,8 @@
+function init() {
+	$("#bg_div").height($(window).height());
+	$("#bg_div").width($(window).width());
+}
+
+$(function(){
+	init();
+});
